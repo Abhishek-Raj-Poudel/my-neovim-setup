02bde71eedb4ad18bb35275771a92053c95190e8
Abhishek Raj Poudel <abhishekrajpoudel@gmail.com>
2024-09-09 19:13:39 +0545

    added some changes in null as by adding mason-null-ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,17 +1,41 @@
 return {
-  {
-    "nvimtools/none-ls.nvim",
-    config = function()
-      local null_ls = require("null-ls")
-      null_ls.setup({
-        sources = {
-          -- add your formatters here , here stylua is a formatter, you need to install it with mason, maybe ensure install in your lspconfig.lua file
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.formatting.prettier,
-          --          null_ls.builtins.diagnostics.eslint_d,
-        },
-      })
+   {
+     "jay-babu/mason-null-ls.nvim",
+       event = { "BufReadPre", "BufNewFile" },
+       dependencies = {
+         "williamboman/mason.nvim",
+         "nvimtools/none-ls.nvim",
+       },
+       config = function()
+         require("mason-null-ls").setup({
+             ensure_installed = { "stylua", "prettier","eslint_d" }
+             })
+
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+            null_ls.builtins.formatting.stylua,
+            null_ls.builtins.formatting.prettier,
+            null_ls.builtins.diagnostics.eslint_d, 
+            }})
       vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
-  },
+     end,
+   },
+    -- {
+    --   "nvimtools/none-ls.nvim",
+    --
+    --   config = function()
+    --   --   local null_ls = require("null-ls")
+    --   --   null_ls.setup({
+    --   --       sources = {
+    --   --       null_ls.builtins.formatting.stylua,
+    --   --       null_ls.builtins.formatting.prettier,
+    --   --       --null_ls.builtins.diagnostics.eslint_d, 
+    --   --       }})
+    --   -- vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    --     end,
+    -- }
 }
+        
+  
+

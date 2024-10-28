32400df41cc36c79eefb22c3c222dafc4812417a
Abhishek Raj Poudel <abhishekrajpoudel@gmail.com>
2024-10-28 21:29:51 +0545

    Updated Theme to gruvbox theme

---
+++ lua/plugins/todo-comment.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/todo-comments.nvim",
+  event = 'VimEnter',
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    signs = false
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
+}

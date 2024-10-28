2ffd089fe2c7f85ffd0d8c128751a9f6eb913072
Abhishek Raj Poudel <abhishekrajpoudel@gmail.com>
2024-10-28 21:37:59 +0545

    Lazy now updates automatically

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,4 +1,4 @@
- -- Bootstrap lazy.nvim
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -30,6 +30,10 @@ require("lazy").setup({
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+  -- automatically check and update for plugin updates
+  checker = {
+    enabled = true,
+    check_interval = 86400,
+    update_on_start = true,
+  },
 })
